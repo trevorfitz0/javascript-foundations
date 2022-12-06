8564ba8ad4a0c2f1d7f39694c3cfed5c5df9c770
trevorfitz0 <trevorfitz0@gmail.com>
2022-12-05 17:23:19 -0700

    All tests Passing

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,22 @@
 class Unicorn {
   constructor(name, color) {
+
+if(color === undefined){
+  this.color = 'white'
+} else {
+  this.color = color;
+}
     this.name = name;
-    this.color = color;
+  }
+  isWhite() {
+    if(this.color === 'white') {
+      return true 
+    } else {
+      return false
+    }
+  }
+  says(phrase) {
+    return(`**;* ${phrase} *;**`)
   }
 }
 
