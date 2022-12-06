9483e33a11d3897ea6369265a14d18ae6efe3907
trevorfitz0 <trevorfitz0@gmail.com>
2022-12-05 18:02:34 -0700

    All Tests Complete

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
 
+class Dragon {
+    constructor(name, rider) {
+        this.name = name
+        this.rider = rider
+        this.timesAte = 0
+        this.hungry = true
+    }
+    greet() {
+        return(`Hi, ${this.rider}!`)
+    }
+    eat() {
+        this.timesAte += 1
+        if( this.timesAte < 3) {
+            this.hungry = true
+        } else {
+            this.hungry = false
+        }
+        return this.hungry
+    }
+}
+
+module.exports = Dragon;
